819d74027155daf5e16158378e92e806cab033aa
Xithrius <15021300+Xithrius@users.noreply.github.com>
2019-07-09 00:11:51 -0700

    The bot is done

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
+<img align="middle" src="icon.jpg">
+
+
 # CrabRaver
 A discord bot that only plays "Crab Rave" by NoiseStorm
+* If a user was to say "Crab Rave" in any channel while being in voice chat, then the bot will join and proceed to play Crab Rave.
+
+## Setup
+
+### Get the song:
+* I got it through the [MonsterCat gold program](https://monster.cat/goldyt), since it's the highest quality of Crab Rave.
+* You can try to do Youtube to MP3, but that's against Youtube's TOS. (Just don't do it, since there's a possibility of getting a virus from one of those websites.)
+
+### Installing requirements:
+|  Operating system  |  Command  |
+| ------------- | ------------- |
+|  Windows  |  `py -3 -m pip install -r requirements.txt`  |
+|  Linux  | `python3 -m pip install -r requirements.txt`  |
+
+### Setting up configuration:
+* Copy `config_example.json` into `config.json`, then replace "Discord token" with your bot token that you get from [Here](https://discordapp.com/developers/applications/) after you create your bot.
+
+### Running the bot:
+* After inviting the bot to your server with [this link](https://discordapp.com/oauth2/authorize?client_id=598030607875833856&scope=bot&permissions=37028928), run the following command, depending on your operating system:
+|  Operating system  |  Command  |
+| ------------- | ------------- |
+|  Windows  |  `py -3 bot.py`  |
+|  Linux  | `python3 bot.py`  |
+
+### All is done, let the chaos begin.
+* The bot should have started up now, enjoy my bot programmed about 90 minutes (bugs included).
---
+++ bot.py
@@ -0,0 +1,80 @@
+"""
+>> CrabRaver
+> Copyright (c) 2019 Xithrius
+> MIT license, Refer to LICENSE for more info
+"""
+
+
+import os
+import json
+import asyncio
+
+from discord.ext import commands as comms
+import discord
+
+from output import path, now, printc
+
+
+class CrabRaver(comms.Bot):
+    """ """
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.config = json.load(open(path('config.json'), 'r'))
+        
+    """ Events """
+
+    async def on_ready(self):
+        printc('[ ! ]: RAVER IS READY TO CRAB')
+
+
+class MainCog(comms.Cog):
+    """ """
+
+    def __init__(self, bot):
+        """ Objects: 
+        comms.Bot subclass as an attribute
+        """
+        self.bot = bot
+
+    """ Events """
+
+    @comms.Cog.listener()
+    async def on_message(self, message):
+        """ """
+        if 'Crab Rave' in message.content:
+            try:
+                vc = await message.author.voice.channel.connect()
+                vc.play(discord.FFmpegPCMAudio(path('music', 'crabrave.mp3')))
+                while vc.is_playing():
+                    await asyncio.sleep(1)
+                vc.stop()
+            except asyncio.TimeoutError:
+                printc(f'Connecting to channel: <{channel}> timed out.')
+
+
+    """ Commands """
+
+    @comms.command()
+    async def icon(self, ctx):
+        """ """
+        await ctx.send(self.bot.user.avatar_url)
+
+    @comms.command()
+    async def song(self, ctx):
+        """ """
+        await ctx.send('https://youtu.be/LDU_Txk06tM')
+
+    @comms.command()
+    @comms.is_owner()
+    async def exit(self, ctx):
+        """ """
+        printc('[WARNING]: RAVER IS LOGGING OUT')
+        await ctx.bot.logout()
+
+
+if __name__ == "__main__":
+    bot = CrabRaver(command_prefix='!', help_command=None)
+    bot.add_cog(MainCog(bot))
+    bot.run(bot.config['discord'], bot=True, reconnect=True)
