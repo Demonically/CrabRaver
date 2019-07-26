d12476241238b9e8ff134af26c11d17f695612d0
Xithrius <15021300+Xithrius@users.noreply.github.com>
2019-07-26 01:17:07 -0700

    Removed console logging

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ from output import path, now, printc
 
 
 class CrabRaver(comms.Bot):
-    """ """
+    """ Subclass """
     
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -43,41 +43,39 @@ class MainCog(comms.Cog):
 
     @comms.Cog.listener()
     async def on_message(self, message):
-        """ """
+        """ Plays Crab Rave when someone says crab rave """
         if 'crab rave' in message.content.lower():
             vc = await message.author.voice.channel.connect()
-            # Source to ffmpeg executable
-            ffmpegSource = path('C:', 'Users', 'Xithr', 'Documents', 'ffmpeg', 'bin', 'ffmpeg.exe')
-            # Music
-            sound = path('music', 'crabrave.mp3')
-            vc.play(discord.FFmpegPCMAudio(executable=ffmpegSource, source=sound))
+            # vc.play(discord.FFmpegPCMAudio(executable=ffmpegSource, source=sound))
+            vc.play(discord.FFmpegPCMAudio(source=path('music', 'crabrave.mp3'), options='-loglevel fatal'))
             vc.source = discord.PCMVolumeTransformer(vc.source)
-            vc.source.volume = 0.1
+            vc.source.volume = 0.025
             while vc.is_playing():
                 await asyncio.sleep(1)
             vc.stop()
             await vc.disconnect()
 
+    @comms.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        """ When a user doesn't do what the crabs want """
+        await ctx.send(f'Crabs have disagreed with your command: {error}')
+
     """ Commands """
 
     @comms.command()
     async def icon(self, ctx):
-        """ """
+        """ Gives the Icon that the Crabs use """
         await ctx.send(self.bot.user.avatar_url)
 
     @comms.command()
     async def song(self, ctx):
-        """ """
+        """ Gives the youtube video of Crab Rave """
         await ctx.send('https://youtu.be/LDU_Txk06tM')
-
-    @comms.command()
-    async def leave(self, ctx):
-        pass # I'll put something here so the bot can leave a voice channel
 
     @comms.command()
     @comms.is_owner()
     async def exit(self, ctx):
-        """ """
+        """ Makes the Crabs go back into their cave """
         printc('[WARNING]: RAVER IS LOGGING OUT')
         await ctx.bot.logout()
 
